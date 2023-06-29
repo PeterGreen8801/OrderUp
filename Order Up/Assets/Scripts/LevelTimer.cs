d9ba02585552f76ea9056804131f0e11815ecf9f
Peter <petergreen880@yahoo.com>
2023-06-29 18:33:04 -0400

    Added Retry Level Screen

--- Order Up/Assets/Scripts/LevelTimer.cs
+++ Order Up/Assets/Scripts/LevelTimer.cs
@@ -4,12 +4,30 @@ using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
 using UnityEngine.EventSystems;
+using Unity.XR;
+using UnityEngine.XR.Interaction.Toolkit.Samples.StarterAssets;
+using UnityEngine.XR.Interaction.Toolkit;
 public class LevelTimer : MonoBehaviour
 {
     [SerializeField]
     public Image fill;
     [SerializeField]
     public TextMeshProUGUI text;
+
+    public DynamicMoveProvider dynamicMoveProvider;
+
+    public TeleportationProvider teleportationProvider;
+
+    public XRRayInteractor xRRayInteractor;
+
+    public XRDirectInteractor LeftxRDirectInteractor;
+    public XRDirectInteractor RightxRDirectInteractor;
+
+    public Image backgroundImage;
+
+    public AudioSource countdownAudioSource;
+
+    public AudioClip timeConfirm;
 
     public int timeLength;
 
@@ -35,8 +53,27 @@ public class LevelTimer : MonoBehaviour
         {
             if (paused == false)
             {
-                text.text = $"{remainingDuration % 59}";
+                if (remainingDuration == 60)
+                {
+                    text.text = $"60";
+                }
+                else
+                {
+                    text.text = $"{remainingDuration % 60}";
+                }
                 fill.fillAmount = Mathf.InverseLerp(0, timeLength, remainingDuration);
+                if (remainingDuration == 40)
+                {
+                    countdownAudioSource.PlayOneShot(timeConfirm);
+                }
+                if (remainingDuration == 20)
+                {
+                    countdownAudioSource.PlayOneShot(timeConfirm);
+                }
+                if (remainingDuration < 6)
+                {
+                    countdownAudioSource.Play();
+                }
                 remainingDuration--;
                 yield return new WaitForSeconds(1f);
             }
@@ -54,6 +91,14 @@ public class LevelTimer : MonoBehaviour
     public void EndOfTimer()
     {
         Debug.Log("End of timer");
+        //Here will freeze player, pop up ui
+        dynamicMoveProvider.enabled = false;
+        teleportationProvider.enabled = false;
+        xRRayInteractor.enabled = false;
+        RightxRDirectInteractor.enabled = false;
+        LeftxRDirectInteractor.enabled = false;
+
+        backgroundImage.gameObject.SetActive(true);
     }
 
     public void startTimer()
