e5e4fb32b673bb0901226e7138ec0f633ae7fbba
Peter <petergreen880@yahoo.com>
2023-06-30 01:17:51 -0400

    Added some NPC Dialog

---
+++ Order Up/Assets/Scripts/NPCDialog.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class NPCDialog : MonoBehaviour
+{
+    public Image npc1dialog;
+
+    public Image npc7dialog;
+
+    public Image npc2dialogfirst;
+    public Image npc2dialog;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("NPC1"))
+        {
+            npc1dialog.gameObject.SetActive(true);
+        }
+        if (other.CompareTag("NPC2") && PlayerPrefs.GetInt("LevelOneHighScore") < 1)
+        {
+            npc2dialogfirst.gameObject.SetActive(true);
+        }
+        if (other.CompareTag("NPC2") && PlayerPrefs.GetInt("LevelOneHighScore") > 0)
+        {
+            npc2dialog.gameObject.SetActive(true);
+        }
+        if (other.CompareTag("NPC7"))
+        {
+            npc7dialog.gameObject.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("NPC1"))
+        {
+            npc1dialog.gameObject.SetActive(false);
+        }
+        if (other.CompareTag("NPC2") && PlayerPrefs.GetInt("LevelOneHighScore") < 1)
+        {
+            npc2dialogfirst.gameObject.SetActive(false);
+        }
+        if (other.CompareTag("NPC2") && PlayerPrefs.GetInt("LevelOneHighScore") > 0)
+        {
+            npc2dialog.gameObject.SetActive(false);
+        }
+        if (other.CompareTag("NPC7"))
+        {
+            npc7dialog.gameObject.SetActive(false);
+        }
+    }
+
+}
