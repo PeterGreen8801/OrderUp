9e5116b1ee97f9924623c45b7d8f9f7c59f55646
Peter <petergreen880@yahoo.com>
2023-08-11 00:54:23 -0400

    Working on LeftorRight Hand Haptics

---
+++ Order Up/Assets/Scripts/LeftOrRightHapticHandProvider.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+using Unity.XR;
+
+public class LeftOrRightHapticHandProvider : MonoBehaviour
+{
+    //public XRBaseControllerInteractor leftHandInteractor;
+    //public XRBaseControllerInteractor rightHandInteractor;
+
+    //public GameObject leftHandInteractor;
+    //public GameObject rightHandInteractor;
+
+    //public XRRayInteractor ray;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+
+    }
+
+}
